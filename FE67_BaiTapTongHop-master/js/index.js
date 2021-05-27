14b8c11d9f84b44aa1d1a2d64e599b3596432614
Tep <phu0nm2@gmail.com>
2021-05-27 16:11:45 +0700

    add marketplace and feedback

--- FE67_BaiTapTongHop-master/js/index.js
+++ FE67_BaiTapTongHop-master/js/index.js
@@ -14,3 +14,97 @@ const handleHeader = () => {
 };
 
 handleHeader();
+
+// slick js cover
+$(function () {
+  $(".cover-content").slick({
+    loop: true,
+    autoplay: true,
+    fade: true,
+    speed: 3000,
+    arrows: false,
+    // dots: true,
+  });
+});
+
+// slick js Services
+$(function () {
+  $(".services-content").slick({
+    infinite: true,
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<span class="priv_arrow"><i class="fas fa-chevron-left"></i></span>',
+    nextArrow:
+      '<span class="next_arrow"><i class="fas fa-chevron-right"></i></span>',
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+// slick js feedback
+$(function () {
+  $(".feedback-content").slick({
+    infinite: true,
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<span class="priv_arrow"><i class="fas fa-chevron-left"></i></span>',
+    nextArrow:
+      '<span class="next_arrow"><i class="fas fa-chevron-right"></i></span>',
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // responsive: [
+    //   {
+    //     breakpoint: 1200,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 992,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 576,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
+  });
+});
